b9894f74088169307a1de9ffc712cf634265fb3b
AnthoDevMoP <bibi35140@hotmail.fr>
2013-10-07 23:48:53 +0200

    Create gate_of_the_setting_sun.h

---
+++ src/server/scripts/Pandaria/GateoftheSettingSun/gate_of_the_setting_sun.h
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2008-2013 TrinityCore <http://www.trinitycore.org/>
+ * Copyright (C) 2013 Arcaniafr<http://arcaniafr.com/>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
+#ifndef DEF_GATE_OF_THE_SETTING_SUN_H
+#define DEF_GATE_OF_THE_SETTING_SUN_H
+
+enum Data
+{
+    DATA_SABOTEUR_KIPTILAK_EVENT,
+    DATA_STRIKER_GADOK_EVENT,
+    DATA_COMMANDER_RIMOK_EVENT,
+    DATA_RAIGONN_EVENT,
+    DATA_TEAM_IN_INSTANCE,
+};
+
+enum Data64
+{
+    DATA_SABOTEUR_KIPTILAK,
+    DATA_STRIKER_GADOK,
+    DATA_COMMANDER_RIMOK,
+    DATA_RAIGONN,
+};
+
+enum CreatureIds
+{
+    //Dungeon Bosses
+
+    BOSS_SABOTEUR_KIPTILAK        = 56906,
+    BOSS_STRIKER_GADOK            = 56589,
+    BOSS_COMMANDER_RIMOK          = 56636,
+    BOSS_RAIGONN                  = 56877,
+
+    //Trash mobs
+
+    NPC_KRIKTHIK_DEMOLISHER     = 56875,
+    NPC_UNLEASHED_SPIRIT        = 65935,
+	
+	//ect not finished
+};
+
+enum GameObjectIds
+{
+};
+
+#endif
